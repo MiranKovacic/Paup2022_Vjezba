f42db121ec40dbe1daf86add49b74f5702c8f28c
Miran Kovačić <miran.kovacic@supersport.hr>
2022-03-15 17:55:12 +0100

    vjezba 3 popis studenata

---
+++ Paup2022_Vjezba/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Paup2022_Vjezba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup2022_Vjezba.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Početna o studentima";
+            ViewBag.Fakultet = "Međimursko veleučilište";
+            return View();
+        }
+
+        public ActionResult Popis()
+        {
+            StudentiDB studentDB = new StudentiDB();
+            return View(studentDB);
+        }
+
+        public ActionResult Detalji(int? id)
+        {
+            if (!id.HasValue)
+                RedirectToAction("Popis");
+
+            StudentiDB studentDB = new StudentiDB();
+
+            Student student = studentDB.VratiListu()
+                .FirstOrDefault(x => x.ID == id);
+
+            if (student == null)
+            {
+                RedirectToAction("Popis");
+            }
+            return View(student);
+        }
+    }
+}
---
+++ Paup2022_Vjezba/Models/StudentiDB.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Paup2022_Vjezba.Models
+{
+    public class StudentiDB
+    {
+        private List<Student> lista = new List<Student>();
+
+        public StudentiDB()
+        {
+            lista.Add(new Student()
+            {
+                ID = 1,
+                Prezime = "Ivić",
+                Ime = "Petar",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1991, 02, 01),
+                OIB = "12345678901",
+                RedobvanStudent = true
+            });
+            lista.Add(new Student()
+            {
+                ID = 2,
+                Prezime = "Mat",
+                Ime = "Milivoj",
+                Spol = 'M',
+                DatumRodjenja = new DateTime(1958, 2, 15),
+                OIB = "10987654321",
+                RedobvanStudent = false
+            });
+            lista.Add(new Student()
+            {
+                ID = 3,
+                Prezime = "Bes",
+                Ime = "Marta",
+                Spol = 'Ž',
+                DatumRodjenja = new DateTime(1987, 11, 12),
+                OIB = "13579086422",
+                RedobvanStudent = true
+            });
+            lista.Add(new Student()
+            {
+                ID = 4,
+                Prezime = "Perko",
+                Ime = "Mia",
+                Spol = 'Ž',
+                DatumRodjenja = new DateTime(1990, 04, 26),
+                OIB = "10293847564",
+                RedobvanStudent = true
+            });
+        }
+
+        public List<Student> VratiListu()
+        {
+            return lista;
+        }
+    }
+}
